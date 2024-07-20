1137775849404ac139fd0284ef902c66cfca18d5
Davyjakim <jakimdavy07@gmail.com>
2024-07-20 12:27:28 +0200

    2nd commit

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+console.log("ENV", process.env.NODE_ENV)
 const app = express();
 app.use(express.json());
 
